803eb359b4e4eb62402810162b462e9653476352
Ari Kohan <ari.kohan.1@gmail.com>
2025-02-06 22:34:28 -0800

    add capacitor first pass

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alaisocial',
+  appName: 'vite_react_shadcn_ts',
+  webDir: 'dist'
+};
+
+export default config;
