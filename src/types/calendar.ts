6ec4c34112edad5c532c8b16cd964ec71f62fc79
Ari Kohan <ari.kohan.1@gmail.com>
2025-03-04 11:04:02 -0500

    fix type issues for chat messages

---
+++ src/types/calendar.ts
@@ -0,0 +1,14 @@
+export interface CalendarEvent {
+    id: string;
+    title: string;
+    description?: string;
+    start_time: string;
+    end_time: string;
+    google_event_id?: string;
+    location?: string;
+    feedback_sent?: boolean;
+    attendees?: Array<{
+      id: string;
+      name: string;
+    }>;
+  }
